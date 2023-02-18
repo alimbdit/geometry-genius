1479372835d760066c9f1be75c7a783e5eeca2b2
alimbdit <alimbdit@gmail.com>
2023-02-18 22:45:40 +0600

    Parallelogram calculation added

--- js/calculation.js
+++ js/calculation.js
@@ -71,7 +71,53 @@ document.getElementById("rectangle-btn").addEventListener("click", function () {
   // tr.setAttribute('id', 'table-row');
 
   tr.innerHTML = `
-    <td>${count + ". Triangle"}</td>
+    <td>${count + ". Rectangle"}</td>
+    <td>${area + " cm&#178;"}</td>
+    <td class="text-center"><button  class="convert-btn bg-buttonBg hover:bg-buttonBgHover px-4 py-2 my-1 rounded-lg text-white items-right">Convert to m<sup>2</sup></button></td>
+    `;
+
+  container.appendChild(tr);
+
+  //  const elements = document.querySelectorAll(".convert-btn");
+  const prntNode = document.getElementById("table-container");
+  const nodes = prntNode.children;
+  for (const node of nodes) {
+    node.children[2].children[0].addEventListener("click", function (event) {
+      // console.log(event.target.parentNode.parentNode);
+      let textValue = event.target.parentNode.parentNode.children[1].innerText;
+      const areaCmString = textValue.substring(0, textValue.indexOf(" cm"));
+      const areaInMeter = convertCmToM(areaCmString);
+      event.target.parentNode.parentNode.children[1].innerHTML = `${areaInMeter + " m&#178;"}` ;
+      console.log(areaInMeter);
+      // console.log(event.target.parentNode.parentNode.children[1].innerText);
+    });
+  }
+
+});
+//------------------------------- calculation for Parallelogram-----------------
+
+document.getElementById("parallelogram-btn").addEventListener("click", function () {
+  const typeParallelogram = 1;
+  const product = getInputFieldValue("parallelogram-b", "parallelogram-h");
+  const area = calculateArea(product, typeParallelogram);
+  count += 1;
+  if (area == "negative") {
+    alert("Your input should not be negative!!!");
+  } else if (area == "oneValue") {
+    alert(
+      "You have  entered only one valid input value. Please enter another one!!!"
+    );
+  } else if (area == "string") {
+    alert("You should enter two number.!!!");
+  } else {
+  }
+
+  const container = document.getElementById("table-container");
+  const tr = document.createElement("tr");
+  // tr.setAttribute('id', 'table-row');
+
+  tr.innerHTML = `
+    <td>${count + ". Parallelogram"}</td>
     <td>${area + " cm&#178;"}</td>
     <td class="text-center"><button  class="convert-btn bg-buttonBg hover:bg-buttonBgHover px-4 py-2 my-1 rounded-lg text-white items-right">Convert to m<sup>2</sup></button></td>
     `;
